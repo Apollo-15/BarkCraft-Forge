32b1af97095402ea503d382538aa725dcf157149
Appolo-13 <rayl1x.gta@gmail.com>
2025-01-07 09:56:03 +0100

    Initial commit

---
+++ src/main/java/net/rayl1x/barkcraft/item/ModCreativeModTabs.java
@@ -0,0 +1,38 @@
+package net.rayl1x.barkcraft.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.rayl1x.barkcraft.BarkCraft;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, BarkCraft.MODID);
+
+    public static final RegistryObject<CreativeModeTab> BARKCRAFT_INGREDIENTS_TAB = CREATIVE_MODE_TABS.register("minefear_ingredients_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.OAK_BARK.get( )))
+                .title(Component.translatable("creativetab.barkcraft_ingredients_tab"))
+                .displayItems((itemDisplayParameters, output) -> {
+                    output.accept(ModItems.OAK_BARK.get());
+                    output.accept(ModItems.BIRCH_BARK.get());
+                    output.accept(ModItems.DARK_OAK_BARK.get());
+                    output.accept(ModItems.SPRUCE_BARK.get());
+                    output.accept(ModItems.ACACIA_BARK.get());
+                    output.accept(ModItems.JUNGLE_BARK.get());
+                    output.accept(ModItems.CHERRY_BARK.get());
+                    output.accept(ModItems.MANGROVE_BARK.get());
+                    output.accept(ModItems.CRIMSON_BARK.get());
+                    output.accept(ModItems.WARPED_BARK.get());
+
+
+                })
+                .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ src/main/java/net/rayl1x/barkcraft/item/ModItems.java
@@ -0,0 +1,50 @@
+package net.rayl1x.barkcraft.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.rayl1x.barkcraft.BarkCraft;
+import net.rayl1x.barkcraft.item.custom.FuelItem;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BarkCraft.MODID);
+
+    public static final RegistryObject<Item> OAK_BARK = ITEMS.register("oak_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> BIRCH_BARK = ITEMS.register("birch_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> ACACIA_BARK = ITEMS.register("acacia_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> CHERRY_BARK = ITEMS.register("cherry_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> DARK_OAK_BARK = ITEMS.register("dark_oak_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> JUNGLE_BARK = ITEMS.register("jungle_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> MANGROVE_BARK = ITEMS.register("mangrove_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> SPRUCE_BARK = ITEMS.register("spruce_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> CRIMSON_BARK = ITEMS.register("crimson_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static final RegistryObject<Item> WARPED_BARK = ITEMS.register("warped_bark",
+            () -> new FuelItem(new Item.Properties(), 200));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+
+}
